49ba620532aea14287102e5c21a4c075518c516e
Garrick Aden-Buie <garrick@adenbuie.com>
2023-10-31 10:05:20 -0400

    add readme, add assets, tweak plots

--- R/plotly_by_hour.R
+++ R/plotly_by_hour.R
@@ -13,7 +13,13 @@ plotly_by_hour <- function(data) {
       type = "histogram"
     ) |>
     plotly::layout(
-      xaxis = list(title = "Hour", range = c(0, 24)),
+      xaxis = list(
+        title = "",
+        range = c(0, 24),
+        dtick = 3,
+        tickvals = seq(0, 24, 3),
+        ticktext = c("12am", paste0(seq(3, 9, 3), "am"), paste0(seq(12, 21, 3), "pm"))
+      ),
       yaxis = list(title = "", overlaying = "y", side = "left"),
       barmode = "stack",
       legend = list(orientation = "h", y = -0.2)
---
+++ README.md
@@ -0,0 +1,4 @@
+
+<h1 align="center">MyNorfolk Dashboard (A Quarto Dash Demo)</h1>
+
+<a href="https://grrrck.quarto.pub/mynorfolk-dash/"><img src="assets/dash-preview.png" align="center" height="400" alt="A preview of the dashboard." /></a>
