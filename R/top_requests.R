ededc9b6a6fd50b14757ef8c7d1217eff9612325
Garrick Aden-Buie <garrick@adenbuie.com>
2023-10-31 09:00:15 -0400

    more plots

--- R/top_requests.R
+++ R/top_requests.R
@@ -30,8 +30,30 @@ plotly_top_requests <- function(norfolk, ..., n = 10) {
     ) |>
     plotly::layout(
       xaxis = list(title = ""),
-      yaxis = list(title = "", ticksuffix = " ", tickfont = list(size = 16)),
-      hoverlabel = list(namelength = 0)
+      yaxis = list(title = "", ticksuffix = " ", tickfont = list(size = 16))
     ) |>
     plotly::config(displayModeBar = F)
+}
+
+plotly_by_hour <- function(data) {
+  data |>
+    mutate(
+      hour = lubridate::hour(creation_date),
+      service_request_category = fct_rev(service_request_category)
+    ) |>
+    plotly::plot_ly(
+      hovertemplate = "%{y}"
+    ) |>
+    plotly::add_histogram(
+      x = ~hour,
+      color = ~ service_request_category,
+      type = "histogram"
+    ) |>
+    plotly::layout(
+      xaxis = list(title = "Hour", range = c(0, 24)),
+      yaxis = list(title = "", overlaying = "y", side = "left"),
+      barmode = "stack",
+      legend = list(orientation = "h", y = -0.2)
+    ) |>
+    plotly::config(displayModeBar = FALSE)
 }
